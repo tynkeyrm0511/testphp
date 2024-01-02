6f06b83de627b646242273e674c7221b350a694f
Nguyen Ngo <hoangnguyen150603@gmail.com>
2024-01-02 22:03:19 +0700

    First commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductsController;
+
+Route::get('products',[
+    ProductsController::class,
+    'index'
+]);
+
+//aaaaaa
+
